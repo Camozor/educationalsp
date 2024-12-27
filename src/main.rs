5ba1f821139e47cc1084cd2d2cdcfdfd1f592606
Camille Vauchel <camille.vauchel@neomi.immo>
2024-12-28 00:40:21 +0100

    Setting up logger

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
-use std::io;
+use log::info;
+use std::{io, process::exit, time::SystemTime};
 
 pub mod core;
 
 fn main() -> io::Result<()> {
-    init_logger();
-    println!("started");
+    if let Err(_) = setup_logger() {
+        eprintln!("Error setting up logger");
+        exit(1);
+    }
+
+    info!("started");
     let stdin = io::stdin();
 
     loop {
         let mut buffer = String::new();
         let size = stdin.read_line(&mut buffer)?;
 
-        println!("{}", size);
-        println!("{}", buffer);
+        info!("{}", size);
+        info!("{}", buffer);
     }
 }
 
-fn init_logger() {}
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {} {}] {}",
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("/tmp/educationalsp.log")?)
+        .apply()?;
+    Ok(())
+}
