f8876968e8f6ce0611b4324bec8829fd1e8d753b
Camille Vauchel <camille.vauchel@neomi.immo>
2024-12-28 02:41:54 +0100

    Start just

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Educationalsp";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      defaultPackage.${system} =
+        pkgs.mkShell { buildInputs = with pkgs; [ just ]; };
+    };
+
+}
